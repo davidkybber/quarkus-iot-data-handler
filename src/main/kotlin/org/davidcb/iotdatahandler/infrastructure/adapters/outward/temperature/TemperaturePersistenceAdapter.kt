feda9bdd95f0c5affa9236d0417c202d8ec5e65a
David Chen Billdal <davidchenbilldal@Davids-MBP.fritz.box>
2022-12-04 20:51:19 +0100

    test: settings for micrometer

--- src/main/kotlin/org/davidcb/iotdatahandler/infrastructure/adapters/outward/temperature/TemperaturePersistenceAdapter.kt
+++ src/main/kotlin/org/davidcb/iotdatahandler/infrastructure/adapters/outward/temperature/TemperaturePersistenceAdapter.kt
@@ -15,8 +15,8 @@ class TemperaturePersistenceAdapter(
     val meterRegistry: MeterRegistry,
 ) : TemperaturePersistencePort {
     override fun persist(temperature: Temperature): Uni<Temperature> {
-        meterRegistry.gauge("temperature", listOf(Tag.of("device", temperature.iotDeviceId)), temperature.degrees.toBigDecimal())
-        meterRegistry.gauge("humidity", listOf(Tag.of("device", temperature.iotDeviceId)), temperature.humidity.toBigDecimal())
+        meterRegistry.gauge(/* name = */ "temperature", /* tags = */ listOf(Tag.of("device", temperature.iotDeviceId)), /* number = */ temperature.degrees.toBigDecimal())
+        meterRegistry.gauge(/* name = */ "humidity", /* tags = */ listOf(Tag.of("device", temperature.iotDeviceId)), /* number = */ temperature.humidity.toBigDecimal())
         val temperatureEntity = temperatureRepository.persist(TemperatureMapper.toEntity(temperature))
         return temperatureEntity.onItem().transform(TemperatureMapper::toDomain)
     }
